6bbff1238f8038ee1b9171b077fc36cbaf110967
Yu Hang Chen <yuhangc9321@hstat.org>
2018-09-28 16:35:30 +0000

    write all five methods

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,20 @@
 #code your answers here
+def check_it_out
+    "Check out my sweet new ride, dude!"
+end
+
+def no_big_deal(car)
+    "No big deal, it's a #{car}"
+end
+
+def stolen
+    "dude, where's my car?"
+end
+
+def reporting_theft(car)
+    "OFFICER, SOMEONE'S STOLEN MY #{car.upcase}"
+end
+
+def how_long_until_i_can_get_another_car(car_price, wage)
+    car_price / wage
+end
